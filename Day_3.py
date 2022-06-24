7c8a45994666ce68037198bee72ba6e6f807b87f
Teja Maganti <tejamaganti96@gmail.com>
2022-06-24 17:03:42 -0400

    Day 3 part 2 finished. Refactored code to improve readability and reduce repeating code

--- Day_3.py
+++ Day_3.py
@@ -2,20 +2,62 @@ with open('inputs/day_3.txt', 'r') as input:
     input_lines = [line.strip() for line in input.readlines()]
 
 
-column_conversion = []
-for row_num in range(len(input_lines[0])):
+def rows_to_columns(binary_values):
+    column_conversion = []
+    for row_num in range(len(binary_values[0])):
         column_conversion.append('')
-        for col_num in range(len(input_lines)):
-            column_conversion[-1] += input_lines[col_num][row_num]
+        for col_num in range(len(binary_values)):
+            column_conversion[-1] += binary_values[col_num][row_num]
 
+    return column_conversion
+
+binary_columns = rows_to_columns(input_lines)
 gamma = ''
-for column in column_conversion:
+for column in binary_columns:
     if column.count('0') > len(column) / 2:
         gamma += '0'
     else:
         gamma += '1'
-
 epsilon = ''.join(map(lambda bit: '1' if bit == '0' else '0', gamma))
 
 print("Submarine power consumption:", int(gamma, 2) * int(epsilon, 2))
 
+oxy_binaries = input_lines.copy()
+oxy_columns = binary_columns.copy()
+oxy_criteria = ''
+i = 0
+while len(oxy_binaries) > 1:
+    if oxy_columns[i].count('1') >= len(oxy_columns[i]) / 2:
+        oxy_criteria += '1'
+    else:
+        oxy_criteria += '0'
+
+    oxy_binaries = list(filter(
+            lambda binary: binary.startswith(oxy_criteria), oxy_binaries
+        )
+    )
+
+    oxy_columns = rows_to_columns(oxy_binaries)
+
+    i += 1
+
+co2_binary = input_lines.copy()
+co2_columns = binary_columns.copy()
+co2_criteria = ''
+i = 0
+while len(co2_binary) > 1:
+    if co2_columns[i].count('1') >= len(co2_columns[i]) / 2:
+        co2_criteria += '0'
+    else:
+        co2_criteria += '1'
+
+    co2_binary = list(filter(
+            lambda binary: binary.startswith(co2_criteria), co2_binary
+        )
+    )
+
+    co2_columns = rows_to_columns(co2_binary)
+
+    i += 1
+
+print("Submarine life support rating:",int(oxy_binaries[0], 2) * int(co2_binary[0], 2))
